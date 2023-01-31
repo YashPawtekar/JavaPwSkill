e69761cc8bd96d5bcbcafcd83798d62d7a178ac3
YashPawtekar <yashpawtekar.8605@gmail.com>
2023-01-31 18:20:04 +0530

    Second commit

---
+++ ScannerClass.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class ScannerClass {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter your name : ");
+        String name = scan.next(); 
+        System.out.println("Name is : " + name);
+
+        System.out.println("Enter your age : ");
+        int age=scan.nextInt(); 
+        System.out.println("Name is : " + age);
+
+        System.out.println("Enter your Number : ");
+        int Number=scan.nextInt(); 
+        System.out.println("Name is : " + Number);
+    }
+}
