651f2e2d957e56cca01308187b2aaaa9c2649a7f
YashPawtekar <yashpawtekar.8605@gmail.com>
2023-02-02 19:00:00 +0530

    2 feb 2023 last commit

--- nestedloop.java
+++ nestedloop.java
@@ -1,8 +1,9 @@
 class nestedloop{
     public static void main(String[] args){
-        for(int i=0; i < 10 ; i++){
-            for(int j=0; j < 10 ; j++){
-                System.out.print("*");
+        int n = 10;
+        for(int i=0; i < n  ; i++){
+            for(int j=0; j < n  ; j++){
+                System.out.print("* ");
             }
             System.out.println("*"); 
         }
