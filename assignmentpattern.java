807f70ec7a56e2610b4c8698e488de4161db7ae0
yash <yashpawtekar.8605@gmail.com>
2023-02-04 19:32:20 +0530

    a to z pattern commit

---
+++ assignmentpattern.java
@@ -0,0 +1,416 @@
+class assignmentpattern {
+    // pattern A 
+    // public static void main(String[] args) {
+    //     int n = 10;
+    //     for (int i = 0; i < n; i++) {
+    //      for(int j=0; j<n; j++){
+    //         if(j==0 && i>0 || i==0 && j>0 && j<n-1 || j==n-1 && i>0 || i==(n-1)/2){
+    //             System.out.print("*");
+    //         }else{
+    //             System.out.print(" ");
+    //         }
+            
+    //      }
+    //      System.out.println(" ");
+    //     }
+    // }
+
+    // pattern B
+    // public static void main(String[] args) {
+    //     int n = 10;
+    //     for (int i = 0; i < n; i++) {
+    //      for(int j=0; j<n; j++){
+    //         if(j==0 || i==0 && j<n-1 || i==n-1 && j<n-1 || i==(n-1)/2 && j<n-1 || j==n-1 && i>0 && i<n-1 ){
+    //             System.out.print("*");
+    //         }else{
+    //             System.out.print(" ");
+    //         }
+            
+    //      }
+    //      System.out.println(" ");
+    //     }
+    // }
+
+    // pattern C 
+
+    // public static void main(String[] args) {
+    //     int n = 10;
+    //     for (int i = 0; i < n; i++) {
+    //      for(int j=0; j<n; j++){
+    //         if(i==0 || j==0 || i==n-1){
+    //             System.out.print("*");
+    //         }else{
+    //             System.out.print(" ");
+    //         }
+            
+    //      }
+    //      System.out.println(" ");
+    //     }
+    // }
+
+    // pattern D
+
+    // public static void main(String[] args) {
+    //     int n = 10;
+    //     for (int i = 0; i < n; i++) {
+    //      for(int j=0; j<n; j++){
+    //         if(j==0 || i==0 && j<n-1 || i==n-1 && j<n-1 || j==n-1 && i>0 && i<n-1){
+    //             System.out.print("*");
+    //         }else{
+    //             System.out.print(" ");
+    //         }
+            
+    //      }
+    //      System.out.println(" ");
+    //     }
+    // }
+
+    // pattern E
+    // public static void main(String[] args) {
+    //         int n = 10;
+    //         for (int i = 0; i < n; i++) {
+    //          for(int j=0; j<n; j++){
+    //             if(i==0 || j==0 || i==n-1 || i==(n-1)/2){
+    //                 System.out.print("*");
+    //             }else{
+    //                 System.out.print(" ");
+    //             }
+                
+    //          }
+    //          System.out.println(" ");
+    //         }
+    // }
+
+     // pattern F
+    //  public static void main(String[] args) {
+    //     int n = 10;
+    //     for (int i = 0; i < n; i++) {
+    //      for(int j=0; j<n; j++){
+    //         if(i==0 || j==0 || i==(n-1)/2){
+    //             System.out.print("*");
+    //         }else{
+    //             System.out.print(" ");
+    //         }
+            
+    //      }
+    //      System.out.println(" ");
+    //     }
+// }
+    
+//pattern G
+//     public static void main(String[] args) {
+//         int n = 10;
+//         for (int i = 0; i < n; i++) {
+//          for(int j=0; j<n; j++){
+//             if(j==0 || i==0 || i==n-1 || j==n-1 && i>(n-1)/2 || i==(n-1)/2 && j>(n-1)/2){
+//                 System.out.print("*");
+//             }else{
+//                 System.out.print(" ");
+//             }
+            
+//          }
+//          System.out.println(" ");
+//         }
+// }
+
+// pattern H 
+// public static void main(String[] args) {
+//     int n = 10;
+//     for (int i = 0; i < n; i++) {
+//      for(int j=0; j<n; j++){
+//         if(j==0 || j==n-1 || i==(n-1)/2){
+//             System.out.print("*");
+//         }else{
+//             System.out.print(" ");
+//         }
+        
+//      }
+//      System.out.println(" ");
+//     }
+// }
+
+// pattern I 
+// public static void main(String[] args) {
+//     int n = 10;
+//     for (int i = 0; i < n; i++) {
+//      for(int j=0; j<n; j++){
+//         if(i==0 || i==n-1 || j==(n-1)/2){
+//             System.out.print("*");
+//         }else{
+//             System.out.print(" ");
+//         }
+        
+//      }
+//      System.out.println(" ");
+//     }
+// }
+
+// pattern J 
+// public static void main(String[] args) {
+//     int n = 10;
+//     for (int i = 0; i < n; i++) {
+//      for(int j=0; j<n; j++){
+//         if(i==0 && j>(n-1)/2 || j==n-1 && i<n-1 || i==n-1 && j<n-1 && j>(n-1)/2){
+//             System.out.print("*");
+//         }else{
+//             System.out.print(" ");
+//         }
+        
+//      }
+//      System.out.println(" ");
+//     }
+// }
+ 
+// pattern K
+// public static void main(String[] args) {
+//     int n = 10;
+//     for (int i = 0; i < n; i++) {
+//      for(int j=0; j<n; j++){
+//         if(j==0 || i+j == (n-1)/2 || i-j ==(n-1)/2){
+//             System.out.print("*");
+//         }else{
+//             System.out.print(" ");
+//         }
+        
+//      }
+//      System.out.println(" ");
+//     }
+// }
+
+
+// pattern L
+// public static void main(String[] args) {
+//     int n = 10;
+//     for (int i = 0; i < n; i++) {
+//      for(int j=0; j<n; j++){
+//         if(j==0 || i==n-1){
+//             System.out.print("*");
+//         }else{
+//             System.out.print(" ");
+//         }
+        
+//      }
+//      System.out.println(" ");
+//     }
+// } 
+
+// pattern M 
+// public static void main(String[] args) {
+//     int n = 10;
+//     for (int i = 0; i < n; i++) {
+//      for(int j=0; j<n; j++){
+//         if(j==0 || j==n-1 || i==j && j<(n-1)/2 || j+i == n-1 && i<(n-1)/2){
+//             System.out.print("*");
+//         }else{
+//             System.out.print(" ");
+//         }
+        
+//      }
+//      System.out.println(" ");
+//     }
+// }
+
+// pattern N 
+
+// public static void main(String[] args) {
+//     int n = 10;
+//     for (int i = 0; i < n; i++) {
+//      for(int j=0; j<n; j++){
+//         if(j==0 || j==n-1 || i==j){
+//             System.out.print("*");
+//         }else{
+//             System.out.print(" ");
+//         }
+        
+//      }
+//      System.out.println(" ");
+//     }
+// }
+
+// pattern O
+
+// public static void main(String[] args) {
+//     int n = 10;
+//     for (int i = 0; i < n; i++) {
+//      for(int j=0; j<n; j++){
+//         if(i==0 && j>0 && j<n-1 || i==n-1 && j>0 && j<n-1 || j==0 && i>0 && i<n-1 || j==n-1 && i>0 && i<n-1){
+//             System.out.print("*");
+//         }else{
+//             System.out.print(" ");
+//         }
+        
+//      }
+//      System.out.println(" ");
+//     }
+// }
+
+// pattern P 
+
+// public static void main(String[] args) {
+//     int n = 10;
+//     for (int i = 0; i < n; i++) {
+//      for(int j=0; j<n; j++){
+//         if(j==0 || i==0 || j==n-1 && i<(n-1)/2 || i==(n-1)/2){
+//             System.out.print("*");
+//         }else{
+//             System.out.print(" ");
+//         }
+        
+//      }
+//      System.out.println(" ");
+//     }
+// }
+
+// pattern Q
+
+// public static void main(String[] args) {
+//     int n = 10;
+//     for (int i = 0; i < n; i++) {
+//      for(int j=0; j<n; j++){
+//         if(i==0 || j==0 && i<n-1 || j==n-1 && i<n-1 || i==n-2 || i==j && j>(n-1)/2){
+//             System.out.print("*");
+//         }else{
+//             System.out.print(" ");
+//         }
+        
+//      }
+//      System.out.println(" ");
+//     }
+// }
+
+// pattern R 
+// public static void main(String[] args) {
+//     int n = 10;
+//     for (int i = 0; i < n; i++) {
+//      for(int j=0; j<n; j++){
+//         if(j==0 || i==0 || j==n-1 && i<(n-1)/2 || i==(n-1)/2 || i-j == (n-1)/2){
+//             System.out.print("*");
+//         }else{
+//             System.out.print(" ");
+//         }
+        
+//      }
+//      System.out.println(" ");
+//     }
+// }
+
+// pattern S 
+// public static void main(String[] args) {
+//     int n = 10;
+//     for (int i = 0; i < n; i++) {
+//      for(int j=0; j<n; j++){
+//         if(i==0 || i==n-1 || j==0 && i<(n-1)/2 || i==(n-1)/2 || j==n-1 && i>(n-1)/2){
+//             System.out.print("*");
+//         }else{
+//             System.out.print(" ");
+//         }
+        
+//      }
+//      System.out.println(" ");
+//     }
+// }
+
+// pattern T
+// public static void main(String[] args) {
+//     int n = 10;
+//     for (int i = 0; i < n; i++) {
+//      for(int j=0; j<n; j++){
+//         if(i==0 || j==(n-1)/2){
+//             System.out.print("*");
+//         }else{
+//             System.out.print(" ");
+//         }
+        
+//      }
+//      System.out.println(" ");
+//     }
+// }
+
+// pattern V 
+// public static void main(String[] args) {
+//     int n = 10;
+//     for (int i = 0; i < n; i++) {
+//      for(int j=0; j<n; j++){
+
+//         if(i==j && j<(n-1)/2 || i+j ==n-1 && j>(n-1)/2){
+//             System.out.print("*");
+//         }else{
+//             System.out.print(" ");
+//         }
+        
+//      }
+//      System.out.println(" ");
+//     }
+// }
+
+// pattern W 
+
+// public static void main(String[] args) {
+//     int n = 10;
+//     for (int i = 0; i < n; i++) {
+//      for(int j=0; j<n; j++){
+
+//         if(j==0 || j==n-1 || i+j ==n-1 && j<(n-1)/2 || i==j && j>(n-1)/2 ){
+//             System.out.print("*");
+//         }else{
+//             System.out.print(" ");
+//         }
+        
+//      }
+//      System.out.println(" ");
+//     }
+// }
+
+// pattern X 
+
+// public static void main(String[] args) {
+//     int n = 10;
+//     for (int i = 0; i < n; i++) {
+//      for(int j=0; j<n; j++){
+
+//         if(i==j || i+j==n-1){
+//             System.out.print("*");
+//         }else{
+//             System.out.print(" ");
+//         }
+        
+//      }
+//      System.out.println(" ");
+//     }
+// }
+// pattern Y
+
+// public static void main(String[] args) {
+//     int n = 10;
+//     for (int i = 0; i < n; i++) {
+//      for(int j=0; j<n; j++){
+
+//         if(i==j && j<(n-1)/2 || i+j==n-1){
+//             System.out.print("*");
+//         }else{
+//             System.out.print(" ");
+//         }
+        
+//      }
+//      System.out.println(" ");
+//     }
+// }
+
+// pattern Z
+
+// public static void main(String[] args) {
+//     int n = 10;
+//     for (int i = 0; i < n; i++) {
+//      for(int j=0; j<n; j++){
+
+//         if(i==0 || i==n-1 || i+j == n-1){
+//             System.out.print("*");
+//         }else{
+//             System.out.print(" ");
+//         }
+        
+//      }
+//      System.out.println(" ");
+//     }
+// }
+}
