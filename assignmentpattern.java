ad0869b9696c68aedef39414d7022a74e81236af
yash <yashpawtekar.8605@gmail.com>
2023-02-04 21:55:17 +0530

    new update

--- assignmentpattern.java
+++ assignmentpattern.java
@@ -326,6 +326,23 @@ class assignmentpattern {
 //     }
 // }
 
+
+// pattern U
+// public static void main(String[] args) {
+//     int n = 10;
+//     for (int i = 0; i < n; i++) {
+//      for(int j=0; j<n; j++){
+//         if(j==0 || j==n-1 || i==n-1){
+//             System.out.print("*");
+//         }else{
+//             System.out.print(" ");
+//         }
+        
+//      }
+//      System.out.println(" ");
+//     }
+// }
+
 // pattern V 
 // public static void main(String[] args) {
 //     int n = 10;
