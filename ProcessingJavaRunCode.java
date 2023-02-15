02825aa092010fd536dd38142fd7ba539d14e852
YashPawtekar <yashpawtekar.8605@gmail.com>
2023-02-15 12:07:22 +0530

    student file

---
+++ ProcessingJavaRunCode.java
@@ -0,0 +1,90 @@
+public class ProcessingJavaRunCode {
+
+// STEP - 1   
+// ------------------------------------
+// Run Code . . . 
+//
+//-------------------------------------
+//
+// STEP - 2   
+// ------------------------------------
+// JAVA compilers . . . 
+//
+//-------------------------------------
+//
+// STEP - 3  
+// ------------------------------------
+// byte Code generate. . . ( .class file generate )
+//
+//-------------------------------------
+//
+// STEP - 4   
+// ------------------------------------
+// 1) JVM execute in hashcode in "1010101010". . . 
+//
+// 2) ( WORA ) Work Ones Run Anywhere
+//
+// 3) " JVM platform depending . . ."  
+//    i) windows
+//    i) Mac
+//    i) linux etc . . .
+//
+//
+// 4) JVM execute in JRE seen below .
+//
+//   <----------J R E---------->
+//                ||
+//                \/
+// --------------------------------
+// |   <--------J V M -------->   |
+// |                              |
+// |                              |
+// |                              |
+// |                              |
+// |                              |
+// |                              |
+// --------------------------------
+//
+// 5) JVM seperate all memories.
+//
+// ".class " file ----> class loder . . .
+//                           ||
+//                           ||
+//                           \/
+//    <---------------------J R E-------------------->
+//                           ||
+//                           \/
+// -------------------------------------------------
+// |            <--------J V M -------->           |
+// | 1) Method Area                                |
+// | 2) Heap Area                                  |
+// | 3) Java Stack                                 |
+// | 4) Pc Registers                               |
+// | 5) Native Methods Stack                       |
+// |                                               |
+// |   //---------Runtime Data Area----------//    |
+// -------------------------------------------------
+//
+//
+//-------------------------------------
+// STEP - 5   
+// ------------------------------------
+// Execute Engine Work - - - -
+//
+// 1) Interpreted & compiler Java is Both
+// 
+// 2) How work java?
+// --> Compile --> Bytecode --> JVM --> Interpreted Start ( Line By Line Execute Bytecode) --> Execute 
+// 
+// 3) JIT Compiler ( Just-In-Time ) execute -->
+//
+//
+//-------------------------------------
+// STEP - 5   
+// ------------------------------------
+// Garbage Collector Execute ---->
+// What garbage collector ?
+// -> Execute after Heap area memory give and deleteing
+// -> Variable not referece to anyone that time execute and collect not referece variable. ( Deallowted to variable)
+
+}
---
+++ classLoadingjava.java
@@ -0,0 +1,38 @@
+public class classLoadingjava {
+
+//
+//                2) STEP 2 
+//
+//                   LINK
+//                ----------
+//                | Verify |
+//  1) STEP 1     ----------
+//--> loading --->    ||
+//   ( J V M )        \/
+//                ----------
+//                | Prepare | --->  i) static Variable ____\  Heap Area Memory allowcated
+//                ----------       ii) static Block        /
+//                    ||
+//                    \/
+//                ----------
+//                | Resolve | ( This part optional)
+//                ----------
+//                  =============================\      3) STEP 3
+//                  =============================/      Initialize ---> 1) static variable
+//                                                                      2) static block
+//                                                                      3) static Method
+//                                                                      4) Non-static variable ( Instance Variable )
+//                                                                      5) Non-static 
+//                                                                      6) Non-static Method
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
+}
