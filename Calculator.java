8c2db15c82e800ba198d34a235c82fd4710bb15b
YashPawtekar <yashpawtekar.8605@gmail.com>
2023-03-13 14:46:47 +0530

    new commit on 13 march 2023

--- Calculator.java
+++ Calculator.java
@@ -3,6 +3,7 @@ interface Cal {
 void display();
  default void square(){}
  default void rectangle(){}
+ default void percentage(){}
 }
 class squareCal implements Cal{
     float side1;
@@ -34,11 +35,27 @@ class rectangleCal implements Cal{
         System.out.println( "You Area of a Rectangle is :- " + side1*side2);
     }
 } 
+class percentageCal implements Cal{
+    float side1;
+    float side2;
+    public void percentage(){
+        System.out.println("Percentage calculator ");
+        System.out.println("Please enter your mark");
+        Scanner scan = new Scanner(System.in);
+        side2 = scan.nextFloat();
+        System.out.println("Please enter total mark");
+        side1 = scan.nextFloat();
+    }
+    public void display(){
+        System.out.println( "You Percentage is :- " + (side2/side1)*100 + "%");
+    }
+} 
 
 class poly{
-   public void polyUse(Cal ref){
+   public void polyUse(Cal ref) {
     ref.square();
     ref.rectangle();
+    ref.percentage();
     ref.display();
    }
 }
@@ -46,19 +63,27 @@ class askType{
     int TypeNum;
     Cal Square=new squareCal();
     Cal rectangle=new rectangleCal();
+    Cal percentage=new percentageCal();
     poly poly = new poly();
     public void askQuestion(){
         System.out.println("1. Area of a Rectangle calculator ");
         System.out.println("2. Square calculator");
-        System.out.println("Please enter number");
+        System.out.println("3. Percentage calculator");
+        System.out.println("Please select any one option");
         Scanner scan= new Scanner(System.in);
         TypeNum = scan.nextInt();
         if(TypeNum == 1){
          poly.polyUse(rectangle);
         }else if(TypeNum == 2){
             poly.polyUse(Square);
-        }else{
-            System.out.println("Please enter correct number"); 
+        }else if(TypeNum == 3){
+            poly.polyUse(percentage);
+        }
+        else{
+            System.out.println("Please enter correct number");
+            System.out.println(""); 
+            System.out.println(""); 
+            askQuestion();
         }
     }
 }
